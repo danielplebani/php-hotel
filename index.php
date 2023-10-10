f2b1414524429e4a2cff757f073aa0d44997d6b1
danielplebani <danielpleba03@gmail.com>
2023-10-10 15:49:59 +0200

    Print all our hotels
     with all available data.
    Start gradually. First print the data on the page, without worrying about the style. Then add Bootstrap and display the information with a table.

---
+++ index.php
@@ -0,0 +1,95 @@
+<!-- 
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate in modo graduale. Prima stampate in pagina i dati, senza preoccuparvi dello stile. Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus:
+-Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+-Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore) 
+-->
+
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hotel</title>
+
+    <!--bootstrap-css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container p-5 d-flex flex-wrap justify-content-center gap-3">
+
+        <?php foreach ($hotels as $key => $hotel): ?>
+            <div class="card" style="width: calc(90% / 3)">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <?= $hotel['name'] ?>
+                    </h5>
+                    <p class="card-text">
+                        <?= $hotel['description'] ?>
+                    </p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        VOTO :
+                        <?= $hotel['vote'] ?>
+                    </li>
+                    <li class="list-group-item">
+                        DISTANZA DAL CENTRO :
+                        <?= $hotel['distance_to_center'] ?>
+                    </li>
+                </ul>
+            </div>
+        <?php endforeach; ?>
+
+    </div>
+</body>
+
+</html>
